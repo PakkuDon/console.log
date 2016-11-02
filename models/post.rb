9d60a82a5b8ce44a7508c7013e8b11289a0a9f0c
PakkuDon <pakku.pd@gmail.com>
2016-11-02 11:39:29 +1100

    Add estimated reading times to posts

--- models/post.rb
+++ models/post.rb
@@ -1,5 +1,13 @@
 require 'active_record'
 
+AVERAGE_WPM = 150
+
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  # Return estimated reading time for post content
+  def estimated_reading_time
+    estimate = content.split(/\s+/).length / AVERAGE_WPM
+    "#{estimate >= 1 ? estimate : '< 1'} min read"
+  end
 end
